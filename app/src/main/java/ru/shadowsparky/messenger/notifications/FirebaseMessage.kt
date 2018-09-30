90e08faf3fbab65b561073ad3ccbc79eae4974d9
Eugene <samsonov_eugene@mail.ru>
2018-09-30 16:56:55 +0300

    test

--- app/src/main/java/ru/shadowsparky/messenger/notifications/FirebaseMessage.kt
+++ app/src/main/java/ru/shadowsparky/messenger/notifications/FirebaseMessage.kt
@@ -18,6 +18,6 @@ class FirebaseMessage : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage?) {
         this.log.print("NEW MESSAGE: ${p0.toString()}")
         super.onMessageReceived(p0)
-        // test
+        // test asdas dqwe wqe
     }
 }
